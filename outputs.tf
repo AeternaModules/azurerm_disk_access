fa19f85bb7f93d6e22e0c3df31b52040e6578593
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:12:30 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,20 @@ output "disk_accesses" {
   description = "All disk_access resources"
   value       = azurerm_disk_access.disk_accesses
 }
+output "disk_accesses_location" {
+  description = "List of location values across all disk_accesses"
+  value       = [for k, v in azurerm_disk_access.disk_accesses : v.location]
+}
+output "disk_accesses_name" {
+  description = "List of name values across all disk_accesses"
+  value       = [for k, v in azurerm_disk_access.disk_accesses : v.name]
+}
+output "disk_accesses_resource_group_name" {
+  description = "List of resource_group_name values across all disk_accesses"
+  value       = [for k, v in azurerm_disk_access.disk_accesses : v.resource_group_name]
+}
+output "disk_accesses_tags" {
+  description = "List of tags values across all disk_accesses"
+  value       = [for k, v in azurerm_disk_access.disk_accesses : v.tags]
+}
 
