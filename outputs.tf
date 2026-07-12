7cf261099592bbdbdf2995c17e6196671fac9cf9
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:21 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "disk_accesses" {
-  description = "All disk_access resources"
-  value       = azurerm_disk_access.disk_accesses
-}
 output "disk_accesses_location" {
-  description = "List of location values across all disk_accesses"
-  value       = [for k, v in azurerm_disk_access.disk_accesses : v.location]
+  description = "Map of location values across all disk_accesses, keyed the same as var.disk_accesses"
+  value       = { for k, v in azurerm_disk_access.disk_accesses : k => v.location }
 }
 output "disk_accesses_name" {
-  description = "List of name values across all disk_accesses"
-  value       = [for k, v in azurerm_disk_access.disk_accesses : v.name]
+  description = "Map of name values across all disk_accesses, keyed the same as var.disk_accesses"
+  value       = { for k, v in azurerm_disk_access.disk_accesses : k => v.name }
 }
 output "disk_accesses_resource_group_name" {
-  description = "List of resource_group_name values across all disk_accesses"
-  value       = [for k, v in azurerm_disk_access.disk_accesses : v.resource_group_name]
+  description = "Map of resource_group_name values across all disk_accesses, keyed the same as var.disk_accesses"
+  value       = { for k, v in azurerm_disk_access.disk_accesses : k => v.resource_group_name }
 }
 output "disk_accesses_tags" {
-  description = "List of tags values across all disk_accesses"
-  value       = [for k, v in azurerm_disk_access.disk_accesses : v.tags]
+  description = "Map of tags values across all disk_accesses, keyed the same as var.disk_accesses"
+  value       = { for k, v in azurerm_disk_access.disk_accesses : k => v.tags }
 }
 
