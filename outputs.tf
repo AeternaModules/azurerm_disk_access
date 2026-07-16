b180ceb1427ab2f1bdd24960e1dc412cb0ec087d
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:47:52 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
 output "disk_accesses_id" {
   description = "Map of id values across all disk_accesses, keyed the same as var.disk_accesses"
-  value       = { for k, v in azurerm_disk_access.disk_accesses : k => v.id }
+  value       = { for k, v in azurerm_disk_access.disk_accesses : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "disk_accesses_location" {
   description = "Map of location values across all disk_accesses, keyed the same as var.disk_accesses"
-  value       = { for k, v in azurerm_disk_access.disk_accesses : k => v.location }
+  value       = { for k, v in azurerm_disk_access.disk_accesses : k => v.location if v.location != null && length(v.location) > 0 }
 }
 output "disk_accesses_name" {
   description = "Map of name values across all disk_accesses, keyed the same as var.disk_accesses"
-  value       = { for k, v in azurerm_disk_access.disk_accesses : k => v.name }
+  value       = { for k, v in azurerm_disk_access.disk_accesses : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "disk_accesses_resource_group_name" {
   description = "Map of resource_group_name values across all disk_accesses, keyed the same as var.disk_accesses"
-  value       = { for k, v in azurerm_disk_access.disk_accesses : k => v.resource_group_name }
+  value       = { for k, v in azurerm_disk_access.disk_accesses : k => v.resource_group_name if v.resource_group_name != null && length(v.resource_group_name) > 0 }
 }
 output "disk_accesses_tags" {
   description = "Map of tags values across all disk_accesses, keyed the same as var.disk_accesses"
-  value       = { for k, v in azurerm_disk_access.disk_accesses : k => v.tags }
+  value       = { for k, v in azurerm_disk_access.disk_accesses : k => v.tags if v.tags != null && length(v.tags) > 0 }
 }
 
